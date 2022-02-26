1703bcfb1e17fd5a658361a96c52c50734d26528
Wasim Akram Gazi <wasim92007@gmail.com>
2022-02-26 13:04:17 -0700

    Update 704_Binary_Search.py

--- 704_Binary_Search.py
+++ 704_Binary_Search.py
@@ -2,10 +2,26 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l,r = 0, len(nums) - 1
 
+        ## Alternative faster solution: Tries to return the
+        ## left point
+        while l < r:
+            m = l + (r-l+1)//2  ## Mid will co-inside with r for n =2
+            #print(f'l:{l}, m:{m}, r:{r}')
+
+            if target < nums[m]:
+                r = m -1
+            else:
+                l = m
+        
+        return l if nums[l] == target else -1
+
+
+
+        ## My First Solution: I try to return the mid point
         ## We will searchin the range
         ## l = r: Takes care of single element array
         while l <= r:
-            m = (l+r)//2
+            m = (l+r)//2 ## Mid will co-inside with l for n = 2
 
             ## We will always check and return mid point
             if nums[m] == target:
@@ -17,4 +33,3 @@ class Solution:
                 r = m -1
         ## We searched entire range and came here means we could not find.
         return - 1
-        
