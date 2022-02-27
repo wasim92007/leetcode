22de28fa315748a4dffbaea5cd883eefe4aae610
Wasim Akram Gazi <wasim@Wasims-MacBook-Air.local>
2022-02-26 17:35:36 -0700

    Binary Search

--- bloomberg/medium/binary-search/33.search_in_rotated_sorted_array.py
+++ bloomberg/medium/binary-search/33.search_in_rotated_sorted_array.py
@@ -1,7 +1,7 @@
 #
 # @lc app=leetcode id=33 lang=python3
 #
-# [33] Search in Rotated Sorted Array
+# [ 33] Search in Rotated Sorted Array
 #
 # https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 #
@@ -54,6 +54,42 @@
 # @lc code=start
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+
+        ## Algorithm, we will try to cast the problem into 
+        ## parts/subcases so that we can perfor binary-search
+        ## on sone specific portion
+
+        ##  The shape of the array will be a saw tooth.
+
+        ## Initialize
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + (r-l+1)//2
+
+            if target == nums[m]:
+                return m
+
+            ## Check if the middle is in left sorted portion:
+            if nums[l] <= nums[m]:
+                ## Check to see if the target is on the right of 
+                ## the middle
+                if target > nums[m] or target < nums[l]:
+                    l = m + 1
+                else:
+                    r = m - 1
+
+            ## The middle is in the right portion
+            else:
+                ## Check to see if the target is on the left of 
+                ## the middle
+                if target < nums[m] or target > nums[r]:
+                    r = m - 1
+                else:
+                    l = m + 1
+                    
+        return -1
+
         
 # @lc code=end
 
